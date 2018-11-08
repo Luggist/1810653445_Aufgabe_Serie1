a85376af2dbc6f937c1ecd77296dd645b975c33b
Stuefer Lukas <1810653445@stud.fh-kufstein.ac.at>
2018-11-08 09:51:58 +0100

    Variable

---
+++ Package3/src/main.java
@@ -0,0 +1,10 @@
+public class main {
+    public static void main(String[] args){
+
+        String name = "Lukas Stuefer";
+        int age = 23;
+        System.out.println(name + " " + age);
+
+    }
+}
+
