fe30d95a262c9b8176e308edaed5e933cc41ff81
Stuefer Lukas <1810653445@stud.fh-kufstein.ac.at>
2018-11-08 10:44:09 +0100

    Aufgabe 5

--- Package2/src/Test2.java
+++ Package2/src/Test2.java
@@ -1,4 +1,4 @@
-public class main {
+public class Test2 {
     public static void main(String [] args){
         System.out.println("FH Kufstein Tirol");
         /* eine Ausgabe */
---
+++ Package3/src/Test3.java
@@ -0,0 +1,25 @@
+public class Test3 {
+
+    public static void main(String[] args){
+
+        String name = "Lukas Stuefer";
+
+        final String FH_Kufstein_Tirol;
+        FH_Kufstein_Tirol = "Fachhochschule Kufstein Tirol";
+        /* Fachhochschule Kufstein Tirol = fhKufsteinTirol = FH_Kufstein_Tirol */
+
+        int age = 23;
+
+        System.out.println(name + " " + age + " " + FH_Kufstein_Tirol);
+
+
+
+        final String yxs;
+
+        System.out.println("Java verwendet für Strings \"doppelte Anführungszeichen\"und einzelne Auführungszeichen wie 'c' um einzelne Zeichen zu definieren ");
+        System.out.println("Druch \\ kann man auch den Backslash selbst beliebig oft ausgeben: \\\\\\\\");
+        System.out.println("Ich \f verwende den\b Backspace, \ndie Newline\n und den \tTabulator ");
+    }
+
+}
+
